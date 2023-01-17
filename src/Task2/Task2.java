887f7b35cf30612286033322cdede8608d3d5c4b
AlcorRoss <sokolovdenic1@mail.ru>
2023-01-17 17:37:34 +0300

    -

---
+++ src/Task2/Task2.java
@@ -0,0 +1,28 @@
+package Task2;
+
+import java.util.List;
+
+public class Task2 {
+    public static void main(String[] args) {
+        List<String> list = ReadInput.readInput();
+        counter(list);
+    }
+
+    public static void counter(List<String> list) {
+        int counter = 0, result = 0;
+
+        for (String s : list) {
+            for (String temp : s.split(" ")) {
+                counter += Integer.parseInt(temp);
+            }
+            if (counter > 1) {
+                result++;
+                counter = 0;
+            } else {
+                counter = 0;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
