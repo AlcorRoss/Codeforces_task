3f8bbbc4414339e6a4ddafafe5001b75a8471182
AlcorRoss <sokolovdenic1@mail.ru>
2023-01-18 17:10:48 +0300

    -

---
+++ src/Task8/Task8.java
@@ -0,0 +1,25 @@
+package Task8;
+
+import java.util.Arrays;
+
+public class Task8 {
+    public static void main(String[] args) {
+        String line = ReadInput.readInput();
+        summandSort(line);
+    }
+
+    public static void summandSort(String line) {
+        StringBuilder st = new StringBuilder();
+
+        String[] temp = line.split("\\+");
+        Arrays.sort(temp);
+
+        for (int i = 0; i < temp.length; i++) {
+            st.append(temp[i]);
+            if (i == temp.length - 1) break;
+            st.append("+");
+        }
+
+        System.out.println(st);
+    }
+}
