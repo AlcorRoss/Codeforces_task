8e05f571f3fe804cb8d9f078c559b1d5dc34b1d0
AlcorRoss <sokolovdenic1@mail.ru>
2023-01-18 17:56:24 +0300

    -

---
+++ src/Task10/ReadInput.java
@@ -0,0 +1,21 @@
+package Task10;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadInput {
+    public static String readInput() {
+        File input = new File("D:\\Java\\Java_work\\Codeforces_task\\src\\Task10\\Input.txt");
+        String line = null;
+
+        try (BufferedReader bf = new BufferedReader(new FileReader(input))) {
+            line = bf.readLine();
+        } catch (IOException e) {
+            System.out.println("Не удалось получить вводные данные");
+            System.out.println(e.getMessage());
+        }
+        return line;
+    }
+}
---
+++ src/Task9/Task8.java
@@ -0,0 +1,12 @@
+package Task9;
+
+public class Task8 {
+    public static void main(String[] args) {
+        String line = ReadInput.readInput();
+        capitalization(line);
+    }
+
+    public static void capitalization(String line) {
+        System.out.println(line.substring(0, 1).toUpperCase() + line.substring(1));
+    }
+}
