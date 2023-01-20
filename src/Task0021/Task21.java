050a12c035068efb6385550a2258a6f254ad99e2
AlcorRoss <sokolovdenic1@mail.ru>
2023-01-20 17:10:14 +0300

    -

---
+++ src/Task0021/Task21.java
@@ -0,0 +1,27 @@
+package Task0021;
+
+import java.util.List;
+
+public class Task21 {
+    public static void main(String[] args) {
+        List<String> stringList = ReadInput.readInput();
+        calculate(stringList);
+    }
+
+    public static void calculate(List<String> stringList) {
+        int coordinateX = 0, coordinateY = 0, coordinateZ = 0;
+
+        for (String s : stringList) {
+            String[] temp = s.split(" ");
+            coordinateX += Integer.parseInt(temp[0]);
+            coordinateY += Integer.parseInt(temp[1]);
+            coordinateZ += Integer.parseInt(temp[2]);
+        }
+
+        if (coordinateX == 0 && coordinateY == 0 && coordinateZ == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
