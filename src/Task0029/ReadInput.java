1eedce48bd828d16a60f049baf6f845380f133a8
AlcorRoss <sokolovdenic1@mail.ru>
2023-01-22 19:08:49 +0300

    -

---
+++ src/Task0029/ReadInput.java
@@ -0,0 +1,27 @@
+package Task0029;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadInput {
+    public static List<String> readInput() {
+        File input = new File("D:\\Java\\Java_work\\Codeforces_task\\src\\Task0029\\Input.txt");
+        List<String> stringList = new ArrayList<>();
+        String line;
+
+        try (BufferedReader bf = new BufferedReader(new FileReader(input))) {
+            bf.readLine();
+            while ((line = bf.readLine()) != null) {
+                stringList.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Не удалось получить вводные данные");
+            System.out.println(e.getMessage());
+        }
+        return stringList;
+    }
+}
