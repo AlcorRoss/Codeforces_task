a614e395d85846b23bc556ae6c1cc9cbb7448a0c
AlcorRoss <sokolovdenic1@mail.ru>
2023-01-24 21:22:32 +0300

    -

--- src/Task0033.java
+++ src/Task0033.java
@@ -4,15 +4,14 @@ public class Task0033 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Set<Integer> tempSet = new HashSet<>();
-        int numberOfStudents, temp;
+        int numberOfStudents, temp, maxOfIdenticalSkills = 0;
 
         int numberOfTests = in.nextInt();
         int[] results = new int[numberOfTests];
 
         for (int i = 0; i < numberOfTests; i++) {
             numberOfStudents = in.nextInt();
-            Integer[] tempArray = new Integer[numberOfStudents + 1];
-            Arrays.fill(tempArray, 0);
+            int[] tempArray = new int[numberOfStudents + 1];
 
             for (int j = 0; j < numberOfStudents; j++) {
                 temp = in.nextInt();
@@ -20,17 +19,20 @@ public class Task0033 {
                 tempArray[temp]++;
             }
 
-            Arrays.sort(tempArray, Collections.reverseOrder());
+            for (int skill : tempArray) {
+                if (skill > maxOfIdenticalSkills) maxOfIdenticalSkills = skill;
+            }
 
-            if (tempArray[0] - 1 >= tempSet.size()) {
+            if (maxOfIdenticalSkills - 1 >= tempSet.size()) {
                 results[i] = tempSet.size();
-            } else if (tempArray[0] <= tempSet.size() - 1) {
-                results[i] = tempArray[0];
+            } else if (maxOfIdenticalSkills <= tempSet.size() - 1) {
+                results[i] = maxOfIdenticalSkills;
             } else {
-                results[i] = tempArray[0] - 1;
+                results[i] = maxOfIdenticalSkills - 1;
             }
 
             tempSet.clear();
+            maxOfIdenticalSkills = 0;
         }
 
         for (int result : results) {
