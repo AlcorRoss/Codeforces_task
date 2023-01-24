cb73d299f131264327907b5db7b084869d666628
AlcorRoss <sokolovdenic1@mail.ru>
2023-01-24 18:14:58 +0300

    -

---
+++ src/Task0032.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Task0032 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String temp;
+        int result = 0, counter = 0, purpose;
+
+        in.nextInt();
+        int numberOfPlayers = in.nextInt();
+        int numberOfDifferences = in.nextInt();
+        int[] armies = new int[numberOfPlayers];
+
+        for (int i = 0; i < numberOfPlayers; i++) {
+            armies[i] = in.nextInt();
+        }
+
+        purpose = in.nextInt();
+
+        for (int army : armies) {
+            temp = Integer.toBinaryString(army ^ purpose);
+            for (int i = 0; i < temp.length(); i++) {
+                if (temp.charAt(i) == '1') counter++;
+            }
+            if (counter <= numberOfDifferences) result++;
+            counter = 0;
+        }
+
+        System.out.println(result);
+    }
+}
