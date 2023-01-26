a75a7f0de80107785130c1aafe141363fe2034aa
AlcorRoss <sokolovdenic1@mail.ru>
2023-01-26 21:16:50 +0300

    -

--- src/Task0038.java
+++ src/Task0038.java
@@ -3,20 +3,17 @@ import java.util.Scanner;
 public class Task0038 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int stick, diamond;
+        int stick, diamond, temp;
         int quantityOfTests = in.nextInt();
         int[] results = new int[quantityOfTests];
 
         for (int i = 0; i < quantityOfTests; i++) {
+
             stick = in.nextInt();
             diamond = in.nextInt();
+            temp = Math.min((stick + diamond) / 3, stick);
+            results[i] = Math.min(temp, diamond);
 
-            if (stick == 0 || diamond == 0) {
-                results[i] = 0;
-                continue;
-            }
-
-            results[i] = (stick + diamond) / 3;
         }
 
         for (int result : results) {
