c8a1fcc07b7f7487806aebcc997991554b4d5723
AlcorRoss <sokolovdenic1@mail.ru>
2023-01-28 11:42:03 +0300

    -

--- src/Task0040_1.java
+++ src/Task0040_1.java
@@ -6,7 +6,7 @@ public class Task0040_1 {
         Scanner in = new Scanner(System.in);
         TreeMap<Integer, Integer> lightsPosition = new TreeMap<>();
         int oldPosition;
-        double maxRange = 0;
+        double maxRange = 0, beginRange;
         int quantityOfLights = in.nextInt();
         int streetLength = in.nextInt();
 
@@ -14,13 +14,23 @@ public class Task0040_1 {
             lightsPosition.put(in.nextInt(), 1);
         }
 
+        System.out.println(lightsPosition);
+
         oldPosition = lightsPosition.firstKey();
         for (int position : lightsPosition.keySet()) {
-            maxRange = Math.max(position - oldPosition, maxRange);
+            System.out.println("Position = " + position);
+            System.out.println("Range = " + (position - oldPosition - 1));
+            maxRange = Math.max(position - oldPosition - 1, maxRange);
+            System.out.println("maxRange = " + maxRange);
             oldPosition = position;
         }
 
-        if (maxRange / 2 >= Math.max(lightsPosition.firstKey(), streetLength - lightsPosition.lastKey())) {
+        beginRange = Math.max(lightsPosition.firstKey(), streetLength - lightsPosition.lastKey());
+
+        System.out.println(maxRange);
+        System.out.println(beginRange);
+
+        if (maxRange / 2 >= beginRange) {
             System.out.println(maxRange / 2);
         } else {
             System.out.println(maxRange);
