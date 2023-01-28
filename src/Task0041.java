e9d70763d7575e633d24dbb212871d8c348cfee4
AlcorRoss <sokolovdenic1@mail.ru>
2023-01-28 14:58:21 +0300

    -

--- src/Task0041.java
+++ src/Task0041.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Task0041 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int result, result_1;
+        int result_1, result_2;
         int numberOfTrips = in.nextInt();
         int seasonPass = in.nextInt();
         int priceOfTrip = in.nextInt();
@@ -12,9 +12,9 @@ public class Task0041 {
         if (priceOfTrip <= (double) priceOfSeasonPass / seasonPass) {
             System.out.println(numberOfTrips * priceOfTrip);
         } else {
-            result = numberOfTrips / seasonPass * priceOfSeasonPass;
-            result_1 = numberOfTrips % seasonPass * priceOfTrip;
-            System.out.println(result + result_1);
+            result_1 = (numberOfTrips / seasonPass * priceOfSeasonPass) + (numberOfTrips % seasonPass * priceOfTrip);
+            result_2 = ((numberOfTrips / seasonPass) + 1) * priceOfSeasonPass;
+            System.out.println(Math.min(result_1, result_2));
         }
     }
 }
