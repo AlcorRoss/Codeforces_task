b790c39c5f86dc026a010d8fb880448dc7530177
AlcorRoss <sokolovdenic1@mail.ru>
2023-01-29 18:01:55 +0300

    -

--- src/Task0043.java
+++ src/Task0043.java
@@ -3,27 +3,37 @@ import java.util.Scanner;
 public class Task0043 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int x = 0, y = 0, counter = 0;
+        int t = in.nextInt();
+        boolean flag;
+        int x, y, counter = 0;
         double result = 0;
 
-        while (result != n) {
-            result = (Math.pow(x, y) * y) + (Math.pow(y, x) * x);
+        for (int i = 0; i < t; i++) {
+            int n = in.nextInt();
+            flag = true;
+            x = y = 0;
 
-            if (counter == 0) {
-                x++;
-                counter = 1;
-            } else if (counter == 1) {
-                y++;
-                counter = 0;
+            while (result != n) {
+                result = (Math.pow(x, y) * y) + (Math.pow(y, x) * x);
+
+                if (result > n || x > n || y > n) {
+                    System.out.println(-1);
+                    flag = false;
+                    break;
+                }
+
+                if (counter == 0) {
+                    x++;
+                    counter = 1;
+                } else if (counter == 1) {
+                    y++;
+                    counter = 0;
+                }
             }
 
-            if (result > n) {
-                System.out.println(-1);
-                break;
+            if (flag) {
+                System.out.println(x + " " + y);
             }
         }
-
-        System.out.println(result);
     }
 }
