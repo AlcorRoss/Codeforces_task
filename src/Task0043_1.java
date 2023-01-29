658f68fd410241dc9bc6b270e9510b9145b1a14f
AlcorRoss <sokolovdenic1@mail.ru>
2023-01-29 19:32:36 +0300

    -

---
+++ src/Task0043_1.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Task0043_1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        boolean flag;
+        int x, y;
+        double result;
+
+        for (int i = 0; i < t; i++) {
+            Map<Integer, Integer> memory = new HashMap<>();
+            int n = in.nextInt();
+            flag = true;
+            x = 1;
+            y = 1;
+
+            while (x <= 20) {
+
+                if (memory.containsKey(x) && memory.get(x) == y || memory.containsKey(y) && memory.get(y) == x) {
+                    y++;
+                    continue;
+                }
+
+                memory.put(x, y);
+                memory.put(y, x);
+
+                result = (Math.pow(x, y) * y) + (Math.pow(y, x) * x);
+
+                if (result == n) {
+                    System.out.println(x + " " + y);
+                    flag = false;
+                    break;
+                }
+
+                if (y == 20) {
+                    x++;
+                    y = 1;
+                }
+                y++;
+            }
+
+            if (flag) {
+                System.out.println(-1);
+            }
+        }
+    }
+}
