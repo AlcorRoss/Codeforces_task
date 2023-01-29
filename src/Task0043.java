d9d94bbdeac89aa81f94b5b6a2e4c8000837d3e6
AlcorRoss <sokolovdenic1@mail.ru>
2023-01-29 18:45:51 +0300

    -

--- src/Task0043.java
+++ src/Task0043.java
@@ -5,29 +5,37 @@ public class Task0043 {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         boolean flag;
-        int x, y;
+        int x, y, counter;
         double result = 0;
 
         for (int i = 0; i < t; i++) {
             int n = in.nextInt();
             flag = true;
             x = y = 1;
+            counter = 0;
 
             while (result != n) {
                 result = (Math.pow(x, y) * y) + (Math.pow(y, x) * x);
 
-                if (x > n) {
+
+
+                if (x >= n || y >= n) {
                     System.out.println(-1);
                     flag = false;
                     break;
                 }
 
-                if (y > n) {
+                if (counter < 19) {
                     x++;
-                    y = 0;
+                    if ((Math.pow(x, y) * y) + (Math.pow(y, x) * x) == n) break;
+                } else {
+                    y++;
+                    x = y;
+                    counter = 0;
+                    if ((Math.pow(x, y) * y) + (Math.pow(y, x) * x) == n) break;
                 }
 
-                y++;
+                counter++;
             }
 
             if (flag) {
