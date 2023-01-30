4b398b5c81b83da92bc4ffdf4dd789738c8adc50
AlcorRoss <sokolovdenic1@mail.ru>
2023-01-30 19:25:06 +0300

    -

--- src/Task0044_2.java
+++ src/Task0044_2.java
@@ -1,9 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Task0044_2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int numberOfTests, temp;
+        Map<Integer, Integer> memoryzationMap = new HashMap<>();
+        int numberOfTests, temp, tempMem;
         int numberOfBox = in.nextInt();
         int[] boxes = new int[numberOfBox + 1];
 
@@ -15,10 +18,18 @@ public class Task0044_2 {
 
         for (int i = 0; i < numberOfTests; i++) {
             temp = in.nextInt();
+
+            if (memoryzationMap.containsKey(temp)) {
+                System.out.println(memoryzationMap.get(temp));
+                continue;
+            }
+
             for (int j = 1; j < boxes.length; j++) {
+                tempMem = temp;
                 temp -= boxes[j];
                 if (temp <= 0) {
                     System.out.println(j);
+                    memoryzationMap.put(tempMem, j);
                     break;
                 }
             }
