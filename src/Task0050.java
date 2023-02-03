1b88c40aba40e2fd5765c2a63e5fcf30e3c4697a
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-03 18:19:41 +0300

    -

---
+++ src/Task0050.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Task0050 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numberOfTests = in.nextInt();
+
+        for (int i = 0; i < numberOfTests; i++) {
+            boolean flag = false;
+            int vertical = 0;
+            int horizontal = 0;
+            int lineLength = in.nextInt();
+            String line = in.nextLine();
+
+            for (int j = 0; j < lineLength; j++) {
+                switch (line.charAt(j)) {
+                    case 'L' -> horizontal--;
+                    case 'R' -> horizontal++;
+                    case 'U' -> vertical++;
+                    case 'D' -> vertical--;
+                }
+
+                if (vertical == 1 && horizontal == 1) {
+                    System.out.println("YES");
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) System.out.println("NO");
+        }
+    }
+}
