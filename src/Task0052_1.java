ea4f2cf8c89286ab6dc243093c40efada8851c4b
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-03 20:48:45 +0300

    -

---
+++ src/Task0052_1.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Task0052_1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numberOfTests = in.nextInt();
+
+        for (int i = 0; i < numberOfTests; i++) {
+            in.nextInt();
+            int result = 0;
+            String line = in.next();
+
+            for (int j = 0; j < line.length() - 1; j++) {
+                Set<Character> st = new HashSet<>();
+                Set<Character> st2 = new HashSet<>();
+
+                char[] chars = line.substring(j, j + 1).toCharArray();
+                char[] chars2 = line.substring(j + 1).toCharArray();
+
+                for (Character c : chars) {
+                    st.add(c);
+                }
+                for (Character c : chars2) {
+                    st2.add(c);
+                }
+
+                result = Math.max(result, st.size() + st2.size());
+            }
+            System.out.println(result);
+        }
+    }
+}
