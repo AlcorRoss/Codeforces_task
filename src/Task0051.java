76807fdeaba48c2be743acc6f3d3823af8c0b5ff
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-03 18:45:29 +0300

    -

---
+++ src/Task0051.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Task0051 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numberOfTests = in.nextInt();
+
+        for (int i = 0; i < numberOfTests; i++) {
+            int lengthAfter = in.nextInt();
+            String line = in.next();
+
+            if (lengthAfter == 0) {
+                System.out.println(0);
+                continue;
+            }
+
+            while (true) {
+                if (line.substring(0, 1).equals(line.substring(line.length() - 1)) || line.length() < 2) {
+                    break;
+                } else {
+                    line = line.substring(1, line.length() - 2);
+                }
+            }
+
+            System.out.println(line.length());
+        }
+    }
+}
