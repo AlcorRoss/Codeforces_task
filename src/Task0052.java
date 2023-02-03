3be885952d97f062a7f2c9b536f617952bfb335b
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-03 19:43:09 +0300

    -

--- src/Task0052.java
+++ src/Task0052.java
@@ -1,4 +1,6 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Task0052 {
     public static void main(String[] args) {
@@ -8,7 +10,7 @@ public class Task0052 {
         for (int i = 0; i < numberOfTests; i++) {
             in.nextInt();
             StringBuilder st = new StringBuilder();
-            StringBuilder st2 = new StringBuilder();
+            Set<Character> st2 = new HashSet<>();
             String line = in.next();
 
             for (int j = 0; j < line.length(); j++) {
@@ -20,13 +22,13 @@ public class Task0052 {
                 }
             }
 
-            for (int j = 0; j < line.length(); j++) {
-                if (st2.indexOf(line.substring(0, 1)) == -1) {
-                    st2.append(line.charAt(0));
-                }
+            char[] array = line.toCharArray();
+
+            for (Character c : array) {
+                st2.add(c);
             }
 
-            System.out.println(st.length() + st2.length());
+            System.out.println(st.length() + st2.size());
         }
     }
 }
