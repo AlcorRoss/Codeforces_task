05ebb1e157c1187c3205b54d27a2c553ee8bfc0a
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-05 18:40:47 +0300

    -

---
+++ src/Task0056.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Task0056 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numberOfTests = in.nextInt();
+
+        for (int i = 0; i < numberOfTests; i++) {
+            int result = 0;
+            int numberOfMonsters = in.nextInt();
+            int[] monsters = new int[numberOfMonsters];
+
+            for (int j = 0; j < monsters.length; j++) {
+                monsters[j] = in.nextInt();
+            }
+
+            Collections.sort(Arrays.asList(monsters), Collections.reverseOrder());
+
+            System.out.println(Arrays.toString(monsters));
+
+            if (monsters[0] != 1) {
+                result = monsters[0] - 1;
+                monsters[0] = 1;
+            }
+
+            for (int j = 1; j < monsters.length; j++) {
+                if (monsters[j] > monsters[j - 1] + 1) {
+                    result += monsters[j] - monsters[j - 1] + 1;
+                }
+            }
+
+            System.out.println(result);
+        }
+    }
+}
