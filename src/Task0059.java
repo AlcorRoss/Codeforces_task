96aece078c503b47f28bef912cce918d37f35fb8
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-08 17:27:07 +0300

    -

---
+++ src/Task0059.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Task0059 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numberOfTests = in.nextInt();
+        int result = 0, temp;
+
+        for (int i = 0; i < numberOfTests; i++) {
+            int numberOfValues = in.nextInt();
+            Map<Integer, Integer> pairs = new HashMap<>();
+
+            for (int j = 0; j < numberOfValues; j++) {
+                temp = in.nextInt();
+                pairs.merge(temp - j, 1, Integer::sum);
+            }
+
+            for (int value : pairs.values()) {
+                if (value > 1) result += value;
+            }
+
+            System.out.println(result);
+        }
+    }
+}
