fe08dfa9fac5c903887209be7c5125d52571f49d
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-09 19:16:47 +0300

    -

--- src/Task0062.java
+++ src/Task0062.java
@@ -5,5 +5,14 @@ public class Task0062 {
         Scanner in = new Scanner(System.in);
         int numberOfTest = in.nextInt();
 
+        for (int i = 0; i < numberOfTest; i++) {
+            int purpose = in.nextInt();
+
+            if (purpose % 2 == 0) {
+                System.out.println(purpose / 2 + " " + purpose / 2);
+            } else {
+                System.out.println(purpose / 2 + " " + (purpose / 2 + 1));
+            }
+        }
     }
 }
