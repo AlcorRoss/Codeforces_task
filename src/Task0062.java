35f4e149ec5070f3feae0c6cbfe806126f4a3b35
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-09 19:00:19 +0300

    -

---
+++ src/Task0062.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Task0062 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numberOfTest = in.nextInt();
+
+    }
+}
