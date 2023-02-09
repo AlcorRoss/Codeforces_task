4aeeb74f9ee458d79bc23544e4a4824c8f03460c
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-09 20:23:46 +0300

    -

--- src/Task0062.java
+++ src/Task0062.java
@@ -19,30 +19,44 @@ public class Task0062 {
 
             String line = String.valueOf(temp);
             String line2 = String.valueOf(temp2);
-
-            if (line.length() == line2.length()) {
-                System.out.println(line + " " + line2);
-                continue;
-            }
-
             char[] chars = line.toCharArray();
             char[] chars2 = line2.toCharArray();
 
-            for (int j = 1; j < chars2.length; j++) {
-                temp = Integer.parseInt(String.valueOf(chars[j - 1])) + Integer.parseInt(String.valueOf(chars2[j]));
+            if (line.length() == line2.length()) {
+                for (int j = 0; j < chars2.length; j++) {
+                    temp = Integer.parseInt(String.valueOf(chars[j])) + Integer.parseInt(String.valueOf(chars2[j]));
 
-                if (temp % 2 == 0) {
-                    chars[j - 1] = Character.forDigit(temp / 2, 10);
-                    chars2[j] = Character.forDigit(temp / 2, 10);
-                    continue;
+                    if (temp % 2 == 0) {
+                        chars[j] = Character.forDigit(temp / 2, 10);
+                        chars2[j] = Character.forDigit(temp / 2, 10);
+                        continue;
+                    }
+
+                    if (j % 2 != 0) {
+                        chars[j] = Character.forDigit(temp / 2 + 1, 10);
+                        chars2[j] = Character.forDigit(temp / 2, 10);
+                    } else {
+                        chars2[j] = Character.forDigit(temp / 2 + 1, 10);
+                        chars[j] = Character.forDigit(temp / 2, 10);
+                    }
                 }
+            } else {
+                for (int j = 1; j < chars2.length; j++) {
+                    temp = Integer.parseInt(String.valueOf(chars[j - 1])) + Integer.parseInt(String.valueOf(chars2[j]));
 
-                if (j % 2 != 0) {
-                    chars[j - 1] = Character.forDigit(temp / 2 + 1, 10);
-                    chars2[j] = Character.forDigit(temp / 2, 10);
-                } else {
-                    chars2[j] = Character.forDigit(temp / 2 + 1, 10);
-                    chars[j - 1] = Character.forDigit(temp / 2, 10);
+                    if (temp % 2 == 0) {
+                        chars[j - 1] = Character.forDigit(temp / 2, 10);
+                        chars2[j] = Character.forDigit(temp / 2, 10);
+                        continue;
+                    }
+
+                    if (j % 2 != 0) {
+                        chars[j - 1] = Character.forDigit(temp / 2 + 1, 10);
+                        chars2[j] = Character.forDigit(temp / 2, 10);
+                    } else {
+                        chars2[j] = Character.forDigit(temp / 2 + 1, 10);
+                        chars[j - 1] = Character.forDigit(temp / 2, 10);
+                    }
                 }
             }
 
