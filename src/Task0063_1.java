d15fdbb3dad9030fd01fd508ac223040cab92263
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-10 19:05:56 +0300

    -

--- src/Task0063_1.java
+++ src/Task0063_1.java
@@ -1,37 +1,57 @@
-import java.util.Arrays;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Task0063_1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int numberOfValues = in.nextInt();
         int[] values = new int[numberOfValues];
-        int[] sortedValues = new int[numberOfValues];
+        TreeMap<Integer, Integer> sortedValues = new TreeMap<>();
 
         for (int i = 0; i < values.length; i++) {
             values[i] = in.nextInt();
-            sortedValues[i] = values[i];
+            sortedValues.merge(values[i], 1, Integer::sum);
         }
 
-        Arrays.sort(sortedValues);
 
         int numberOfTests = in.nextInt();
 
         for (int i = 0; i < numberOfTests; i++) {
             int type = in.nextInt();
-            int origin = in.nextInt() - 1;
-            int bound = in.nextInt() - 1;
+            int origin = in.nextInt();
+            int bound = in.nextInt();
             long result = 0;
+            int temp = 0, corrector;
+            boolean flag = true;
 
             switch (type) {
                 case (1) -> {
-                    for (int j = origin; j <= bound; j++) {
+                    for (int j = origin - 1; j <= bound - 1; j++) {
                         result += values[j];
                     }
                 }
                 case (2) -> {
-                    for (int j = origin; j <= bound; j++) {
-                        result += sortedValues[j];
+                    for (var pair : sortedValues.entrySet()) {
+                        if (temp < origin) {
+                            temp += pair.getValue();
+                        } else if (temp > origin && flag) {
+                            temp += pair.getValue();
+                            flag = false;
+                            corrector = (temp - origin) * pair.getKey();
+                            result += (long) pair.getKey() * pair.getValue() - corrector;
+                        } else if (temp >= origin && temp < bound) {
+                            temp += pair.getValue();
+                            flag = false;
+                            result += (long) pair.getKey() * pair.getValue();
+                        } else if (temp == bound) {
+                            result += pair.getKey();
+                            break;
+                        } else {
+                            corrector = (temp - bound) * pair.getKey();
+                            result += (long) pair.getKey() * pair.getValue();
+                            result -= corrector;
+                            break;
+                        }
                     }
                 }
             }
