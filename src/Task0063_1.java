817c4cf5bf6bf77fb9bb3a99e2de73d9cd0b6271
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-10 19:42:32 +0300

    -

--- src/Task0063_1.java
+++ src/Task0063_1.java
@@ -5,8 +5,8 @@ public class Task0063_1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int numberOfValues = in.nextInt();
-        int[] values = new int[numberOfValues];
-        int[] sortedValues = new int[numberOfValues];
+        long[] values = new long[numberOfValues];
+        long[] sortedValues = new long[numberOfValues];
 
         for (int i = 0; i < values.length; i++) {
             values[i] = in.nextInt();
@@ -15,28 +15,23 @@ public class Task0063_1 {
 
         Arrays.sort(sortedValues);
 
+        for (int i = 1; i < values.length; i++) {
+            values[i] += values[i - 1];
+            sortedValues[i] += sortedValues[i - 1];
+        }
+
         int numberOfTests = in.nextInt();
 
         for (int i = 0; i < numberOfTests; i++) {
             int type = in.nextInt();
             int origin = in.nextInt() - 1;
             int bound = in.nextInt() - 1;
-            long result = 0;
 
             switch (type) {
-                case (1) -> {
-                    for (int j = origin; j <= bound; j++) {
-                        result += values[j];
-                    }
-                }
-                case (2) -> {
-                    for (int j = origin; j <= bound; j++) {
-                        result += sortedValues[j];
-                    }
-                }
+                case (1) -> System.out.println(values[bound] - values[origin]);
+                case (2) -> System.out.println(sortedValues[bound] - sortedValues[origin]);
             }
-
-            System.out.println(result);
         }
+
     }
 }
