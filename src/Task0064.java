f0eb8dee74f878224a144da58079dbfa6ec4c7da
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-11 19:16:29 +0300

    -

--- src/Task0064.java
+++ src/Task0064.java
@@ -20,9 +20,7 @@ public class Task0064 {
                 System.out.println(x3 + " " + y3 + " " + x4 + " " + y4);
             else
                 System.out.println(-1);
-        }
-
-        if (y1 == y2) {
+        } else if (y1 == y2) {
             if (x1 < x2) length = x2 - x1;
             else length = x1 - x2;
 
@@ -33,16 +31,16 @@ public class Task0064 {
                 System.out.println(x3 + " " + y3 + " " + x4 + " " + y4);
             else
                 System.out.println(-1);
+        } else {
+            x4 = x1;
+            y4 = y2;
+            x3 = x2;
+            y3 = y1;
+
+            if (Math.max(x3, x1) - Math.min(x3, x1) != Math.max(y4, y1) - Math.min(y4, y1))
+                System.out.println(-1);
+            else
+                System.out.println(x3 + " " + y3 + " " + x4 + " " + y4);
         }
-
-        x4 = x1;
-        y4 = y2;
-        x3 = x2;
-        y3 = y1;
-
-        if (Math.max(x3, x1) - Math.min(x3, x1) != Math.max(y4, y1) - Math.min(y4, y1))
-            System.out.println(-1);
-        else
-            System.out.println(x3 + " " + y3 + " " + x4 + " " + y4);
     }
 }
