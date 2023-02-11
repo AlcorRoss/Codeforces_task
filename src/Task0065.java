d910a0e1034cb39c5fd26ba08591177034298af0
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-11 23:23:47 +0300

    -

--- src/Task0065.java
+++ src/Task0065.java
@@ -7,7 +7,7 @@ public class Task0065 {
 
         for (int i = 0; i < numberOfTests; i++) {
             int numberOfDigit = in.nextInt();
-            boolean flag = true;
+            boolean flag = true, flag2 = false;
             StringBuilder st1 = new StringBuilder().append(1);
             StringBuilder st2 = new StringBuilder().append(1);
             String line = in.next();
@@ -20,6 +20,7 @@ public class Task0065 {
                         st2.append(0);
                     }
                     case ('1') -> {
+                        flag2 = true;
                         if (flag) {
                             st1.append(2);
                             st2.append(1);
@@ -30,13 +31,12 @@ public class Task0065 {
                         }
                     }
                     case ('2') -> {
-                        if (flag) {
-                            st1.append(2);
-                            st2.append(0);
-                            flag = false;
-                        } else {
+                        if (flag2) {
                             st1.append(0);
                             st2.append(2);
+                        } else {
+                            st1.append(1);
+                            st2.append(1);
                         }
                     }
                 }
