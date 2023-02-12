784d926b906bd88ce21683a909b41f7e0707c9d7
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-12 15:06:41 +0300

    -

--- src/Task0067_3.java
+++ src/Task0067_3.java
@@ -15,20 +15,25 @@ public class Task0067_3 {
         for (int i = 0; i < numberOFDays; i++) {
             int origin = in.nextInt();
             int bound = in.nextInt();
-            int minDif = Integer.MAX_VALUE, minDif2 = Integer.MAX_VALUE;
+            int minDif = Integer.MAX_VALUE, minDif2 = Integer.MAX_VALUE, maxHeight = 0;
 
             if ((bound - origin) == 1) {
                 System.out.println(Math.abs(height[origin] - height[bound]));
                 continue;
             }
 
-            for (int j = origin + 1; j <= bound; j++) {
-                minDif = Math.min(minDif, height[origin] - height[j]);
+            for (int j = origin; j <= bound; j++) {
+                maxHeight = Math.max(maxHeight, height[j]);
             }
 
-            for (int j = origin + 1; j <= bound; j++) {
-                if ((height[origin] - height[j]) > minDif)
-                    minDif2 = Math.min(minDif2, height[origin] - height[j]);
+            for (int j = origin; j <= bound; j++) {
+                if ((maxHeight - height[j]) != 0)
+                    minDif = Math.min(minDif, maxHeight - height[j]);
+            }
+
+            for (int j = origin; j <= bound; j++) {
+                if ((height[origin] - height[j]) > minDif && (maxHeight - height[j]) != 0)
+                    minDif2 = Math.min(minDif2, maxHeight - height[j]);
             }
 
             System.out.println(Math.abs(Math.abs(minDif) - Math.abs(minDif2)));
