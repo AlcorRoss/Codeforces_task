580162eb25aacb92c8c01f532b3f0037a0c94fbe
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-12 12:20:10 +0300

    -

--- src/Task0066.java
+++ src/Task0066.java
@@ -10,7 +10,7 @@ public class Task0066 {
             long secondDayPrice = in.nextLong();
             long purpose = in.nextLong();
             long quantityOfDiscount = in.nextLong();
-            long temp1, temp2, temp3, tempDiscount;
+            long temp1, temp2, temp3, temp4, tempDiscount;
 
             temp1 = purpose * firstDayPrice;
             temp2 = purpose * secondDayPrice;
@@ -20,16 +20,21 @@ public class Task0066 {
                 continue;
             }
 
-            if (purpose == quantityOfDiscount + 1) {
-                temp1 = Math.min(temp1, firstDayPrice * quantityOfDiscount);
+            if (purpose % (quantityOfDiscount + 1) == 0) {
+                temp3 = Math.max(1, purpose / quantityOfDiscount + 1);
+                temp1 = Math.min(temp1, firstDayPrice * quantityOfDiscount * temp3);
                 temp1 = Math.min(temp1, temp2);
                 System.out.println(temp1);
                 continue;
             }
 
-            temp3 = purpose - quantityOfDiscount - 1;
-            tempDiscount = (temp3 * firstDayPrice) + (quantityOfDiscount * firstDayPrice);
-            tempDiscount = Math.min(tempDiscount, (temp3 * secondDayPrice) + (quantityOfDiscount * firstDayPrice));
+
+            temp3 = purpose / (quantityOfDiscount + 1);
+            temp4 = purpose % (quantityOfDiscount + 1);
+            temp3 *= quantityOfDiscount;
+
+            tempDiscount = (temp3 * firstDayPrice) + (temp4 * firstDayPrice);
+            tempDiscount = Math.min(tempDiscount, (temp4 * secondDayPrice) + (temp3 * firstDayPrice));
 
             temp1 = Math.min(temp1, temp2);
             temp1 = Math.min(temp1, tempDiscount);
