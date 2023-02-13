d5b85211f6b8a97b3531bdd5043691991b82ca67
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-13 23:26:01 +0300

    -

--- src/Task0069_2.java
+++ src/Task0069_2.java
@@ -2,20 +2,22 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Task0069_1 {
+public class Task0069_2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int numberOfTests = in.nextInt();
 
         for (int i = 0; i < numberOfTests; i++) {
             int numberOfValues = in.nextInt();
-            int averageValue = 0, temp;
+            int averageValue = 0, temp, result = 0;
             double averageWeight;
             Map<Integer, Integer> valueMap = new HashMap<>();
+            Map<Integer, Integer> weightMap = new HashMap<>();
 
             for (int j = 0; j < numberOfValues; j++) {
                 temp = in.nextInt();
                 valueMap.merge(temp, 1, Integer::sum);
+                weightMap.merge(temp, 1, Integer::sum);
                 averageValue += temp;
             }
 
@@ -28,10 +30,10 @@ public class Task0069_1 {
             }
 
             averageWeight = Math.round((double) averageValue / (double) numberOfValues);
+            averageWeight *= 2;
 
-            averageValue = (int) averageWeight;
+            averageValue /= numberOfValues;
             averageValue *= 2;
-            System.out.println(averageValue);
             temp = 0;
 
             for (int value : valueMap.keySet()) {
@@ -47,7 +49,26 @@ public class Task0069_1 {
                 valueMap.put(value, 0);
             }
 
-            System.out.println(temp);
+            averageValue = (int) averageWeight;
+
+            for (int value : weightMap.keySet()) {
+                if (averageValue - value > 0 && weightMap.containsKey(averageValue - value)
+                        && weightMap.get(averageValue - value) != 0) {
+                    if ((averageValue - value) == value) {
+                        result += weightMap.get(value) / 2;
+                    } else {
+                        result += Math.min(weightMap.get(value), weightMap.get(averageValue - value));
+                    }
+                    weightMap.put(averageValue - value, 0);
+                }
+                weightMap.put(value, 0);
+            }
+
+            result = Math.max(result, temp);
+
+            System.out.println(result);
         }
     }
 }
+
+//Продолжи это. Неправильный ответ в тесте 2.
