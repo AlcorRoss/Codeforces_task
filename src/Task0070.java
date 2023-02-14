cece5115900cb0ae25987d78422f2888a9dee8b5
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-14 18:39:47 +0300

    -

--- src/Task0070.java
+++ src/Task0070.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Task0070 {
@@ -6,7 +7,7 @@ public class Task0070 {
         Scanner in = new Scanner(System.in);
         int numberOfValues = in.nextInt();
         int purpose = in.nextInt();
-        long[] values = new long[numberOfValues];
+        Long[] values = new Long[numberOfValues];
 
         for (int i = 0; i < numberOfValues; i++) {
             values[i] = in.nextLong();
@@ -14,11 +15,11 @@ public class Task0070 {
 
         Arrays.sort(values);
         if (values.length == 1) {
-            if (values[0] <= purpose)
-                System.out.println(values[0]);
-            else
-                System.out.println(-1);
-        } else if (values[purpose - 1] != values[purpose]) {
+            if (purpose == 0) System.out.println(values[0] - 1);
+            else System.out.println(values[0]);
+        } else if (purpose == 0) {
+            System.out.println(Collections.min(Arrays.asList(values)) - 1);
+        } else if (values[purpose - 1].equals(values[purpose])) {
             System.out.println(values[purpose - 1]);
         } else {
             System.out.println(-1);
