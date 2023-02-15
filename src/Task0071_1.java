2ba9f54b73927225d98ca1f70034f723a9ecd40b
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-15 19:32:34 +0300

    -

---
+++ src/Task0071_1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Task0071_1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numberOfTests = in.nextInt();
+
+        for (int i = 0; i < numberOfTests; i++) {
+            long firstValue = in.nextLong();
+            long purposeValue = in.nextLong();
+            long temp = firstValue;
+
+            for (int j = 1; j < purposeValue; j++) {
+                temp = calculateNextValue(temp);
+            }
+            System.out.println(temp);
+        }
+    }
+
+    public static long calculateNextValue(long temp) {
+        long temp2 = temp;
+        long min = 10, max = 0;
+
+        while (temp2 > 0) {
+            min = Math.min(min, temp2 % 10);
+            max = Math.max(max, temp2 % 10);
+            temp2 /= 10;
+        }
+        return temp + (min * max);
+    }
+}
