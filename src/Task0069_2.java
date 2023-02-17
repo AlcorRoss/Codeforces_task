112c8bfa08c7ca29135639d7a8c42e02d7729f11
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-17 18:30:57 +0300

    -

--- src/Task0069_2.java
+++ src/Task0069_2.java
@@ -5,26 +5,30 @@ public class Task0069_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int maxTestsCount = sc.nextInt();
-
         for (int i = 0; i < maxTestsCount; i++) {
             int maxMemberCount = sc.nextInt();
-            int[] weights = new int[105];
+            int[] weights = new int[105]; //да,беру допущение,что максимальный вес человека у нас - это 105 кг, идеальная вселенная))
 
             for (int j = 0; j < maxMemberCount; j++) {
                 weights[sc.nextInt()]++;
             }
 
             int max = 0;
-            for (int j = 1; j <= 105; j++) {
+            //иду по числу количества весов всех участников,
+            // т.е. для последовательности: 1 1 3 4 4 2 мой массив будет иметь вид 0 2 1 1 2
+            for (int j = 1; j <= 104; j++) {
                 int counter = 0;
-                for (int k = 1; k <= j / 2; k++) {
-                    if (j - k != k) {
-                        counter += Math.min(weights[k], weights[j - k]);
-                    }
+                //принимаю каждый вес как максимальный и пытаюсь сосчитать число пар с ним
+                //имеет смысл считать только те веса,что остались слева,т.е. для 3,например - это будет вес 1 и 2
+                //каждый раз считаю,что полученное число пар - максимальное и иду дальше к следующему весу
+                //имеет смысл проходиться в k цикле только до половины индексов j,т.к. дальше сочетания начнут повторяться
+                //причем тут важно именно сочетание k и j-k, т.е. подибра
+                for (int k = 1; k <= j; k++) {
+                    counter += Math.min(weights[k], weights[j - k]);
                 }
                 max = Math.max(counter, max);
             }
-            System.out.println(max);
+            System.out.println(max/2);
         }
     }
 }
