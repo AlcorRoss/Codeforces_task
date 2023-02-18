cf7cdbd331feba6c0b8409c632437da7ef5fa155
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-18 18:49:31 +0300

    -

--- src/Task0076_CF1472D.java
+++ src/Task0076_CF1472D.java
@@ -1,5 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task0076_CF1472D {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numberOfTests = in.nextInt();
 
+        while (numberOfTests-- > 0) {
+            int numberOfValues = in.nextInt();
+            int counterBob = 0, counterAlice = 0;
+            int[] values = new int[numberOfValues];
+
+            for (int i = 0; i < numberOfValues; i++) {
+                values[i] = in.nextInt();
+            }
+            Arrays.sort(values);
+
+            for (int i = 0; i < numberOfValues; i++) {
+                if (i % 2 == 0) {
+                    if (values[i] % 2 == 0) counterAlice += values[i];
+                } else {
+                    if (values[i] % 2 != 0) counterBob += values[i];
+                }
+            }
+
+            if (counterAlice > counterBob) {
+                System.out.println("Alice");
+            } else if (counterBob > counterAlice) {
+                System.out.println("Bob");
+            } else {
+                System.out.println("Tie");
+            }
+        }
     }
 }
