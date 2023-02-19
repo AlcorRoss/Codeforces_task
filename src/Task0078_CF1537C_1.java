d2a033d18bdcbd59284f3be6c79154dafac79e40
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-19 18:40:44 +0300

    -

---
+++ src/Task0078_CF1537C_1.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Task0078_CF1537C_1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numberOfTests = in.nextInt();
+
+        while (numberOfTests-- > 0) {
+            int minDiff = Integer.MAX_VALUE, temp;
+            int minDiffIndex = 0;
+            int numberOfValues = in.nextInt();
+            int[] values = new int[numberOfValues];
+
+            for (int i = 0; i < numberOfValues; i++) {
+                values[i] = in.nextInt();
+            }
+            Arrays.sort(values);
+
+            for (int i = 1; i < values.length; i++) {
+                temp = values[i] - values[i - 1];
+                if (temp < minDiff) {
+                    minDiff = temp;
+                    minDiffIndex = i;
+                }
+            }
+
+            for (int i = minDiffIndex; i < values.length; i++) {
+                System.out.print(values[i] + " ");
+            }
+
+            for (int i = 0; i < minDiffIndex; i++) {
+                if (i != minDiffIndex - 1) System.out.print(values[i] + " ");
+                else System.out.println(values[i]);
+            }
+        }
+    }
+}
