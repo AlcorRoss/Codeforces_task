25bc2e62d271bbce30bf5c9f9c6f99e3d5badcb6
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-19 16:33:58 +0300

    -

--- src/Task0077_CF1369B.java
+++ src/Task0077_CF1369B.java
@@ -6,58 +6,17 @@ public class Task0077_CF1369B {
         int numberOfTests = in.nextInt();
 
         while (numberOfTests-- > 0) {
-            boolean flag = false, flag1 = false;
             in.nextInt();
-            StringBuilder line = new StringBuilder(in.next());
+            int firstOne = -1, lastZero = -1;
+            String line = in.next();
 
-            for (int i = line.length() - 1; i > 0; i--) {
-                if (line.charAt(i) == '0') flag = true;
-                if (line.charAt(i) == '1' && flag) line.replace(i, i + 1, "");
-            }
-
-            flag = false;
             for (int i = 0; i < line.length(); i++) {
-                if (line.charAt(i) == '1') flag = true;
-                if (line.charAt(i) == '0' && !flag1) {
-                    flag1 = true;
-                    continue;
-                }
-                if (line.charAt(i) == '0' && flag && flag1) {
-                    line.replace(i, i + 1, "");
-                    i -= 1;
-                }
+                if (line.charAt(i) == '1' && firstOne == -1) firstOne = i;
+                else if (firstOne != -1) lastZero = i;
             }
 
-            flag = false;
-            for (int i = line.length() - 1; i >= 0; i--) {
-                if (line.charAt(i) == '0') flag = true;
-                if (line.charAt(i) == '1' && flag) line.replace(i, i + 1, "");
-            }
-
-            System.out.println(line);
+            if (firstOne == -1 || lastZero == -1) System.out.println(line);
+            else System.out.println(line.substring(0, firstOne) + line.substring(lastZero));
         }
     }
 }
-
-// 11001101 →
-// 1100101 →
-// 110101 →
-// 10101 →
-// 1101 →
-// 101 →
-// 01.
-
-// 11001101 →  если i!=0, то удаляем 1 с конца после первого 0
-// 11001101 →
-// 1100101 →
-// 110001 →
-// 10001 →
-
-// 10001 → с начала. Удаляем 0 после первой единицы, если следующий 0 и i!=длине строки
-// 1001 →
-// 101 →
-
-// 101 → идем с конца, удаляем единицы после 0
-// 01 →
-
-//выводим результат
