94bb5554a9d962ff2006818f4e17c52c86706eac
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-20 12:56:10 +0300

    -

---
+++ src/Task0079_CF1334A.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Task0079_CF1334A {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numberOfTests = in.nextInt();
+
+        while (numberOfTests-- > 0) {
+            boolean flag = true;
+            int numberOfValues = in.nextInt();
+            int plays, clears, temp1, temp2, tempPlays = 0, tempClears = 0;
+
+            while (numberOfValues-- > 0) {
+                plays = in.nextInt();
+                clears = in.nextInt();
+                temp1 = plays - tempPlays;
+                temp2 = clears - tempClears;
+
+                if (temp2 < 0 || temp2 > temp1) {
+                    System.out.println("NO");
+                    flag = false;
+                    break;
+                }
+                tempPlays = plays;
+                tempClears = clears;
+            }
+
+            if (flag) System.out.println("YES");
+        }
+    }
+}
