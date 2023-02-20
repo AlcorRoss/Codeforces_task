2cb43e4bc16760fef6b0c65fcf30f1354f55274a
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-20 13:45:54 +0300

    -

--- src/Task0079_CF1334A.java
+++ src/Task0079_CF1334A.java
@@ -16,7 +16,7 @@ public class Task0079_CF1334A {
                 temp1 = plays - tempPlays;
                 temp2 = clears - tempClears;
 
-                if (temp2 < 0 || temp2 > temp1) {
+                if (temp2 < 0 || temp2 > temp1 && flag) {
                     System.out.println("NO");
                     flag = false;
                 }
