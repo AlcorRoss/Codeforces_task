7e76137763731da1164b4a5820e84e8670e590ab
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-21 19:06:16 +0300

    -

--- src/Task0082_CF4B.java
+++ src/Task0082_CF4B.java
@@ -7,14 +7,43 @@ public class Task0082_CF4B {
         int sumTime = in.nextInt();
         int[] min = new int[numberOfDays];
         int[] max = new int[numberOfDays];
-        int temp = 0;
+        int temp = 0, temp2;
+        boolean flag = false;
 
         for (int i = 0; i < numberOfDays; i++) {
             min[i] = in.nextInt();
             max[i] = in.nextInt();
-            temp += max[i] - min[i];
+            temp += max[i];
         }
 
-        if(temp<sumTime) System.out.println("NO");
+        if (temp < sumTime) {
+            System.out.println("NO");
+        } else if (temp == sumTime) {
+            System.out.println("YES");
+            for (int value : max) System.out.print(value + " ");
+        } else {
+            temp -= sumTime;
+            for (int i = 0; i < max.length; i++) {
+                temp2 = max[i] - min[i];
+                if (temp > temp2) {
+                    temp -= temp2;
+                    max[i] = min[i];
+                } else if (temp < temp2) {
+                    max[i] -= temp2 - temp;
+                    flag = true;
+                    break;
+                } else {
+                    max[i] = min[i];
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) {
+                System.out.println("YES");
+                for (int value : max) System.out.print(value + " ");
+            } else {
+                System.out.println("NO");
+            }
+        }
     }
 }
