38391cffa8e0d998014fb51c7c005ff213a4324d
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-21 15:51:23 +0300

    -

--- src/Task0080_CF1409C.java
+++ src/Task0080_CF1409C.java
@@ -9,14 +9,39 @@ public class Task0080_CF1409C {
             int arrayLength = in.nextInt();
             int firstValue = in.nextInt();
             int secondValue = in.nextInt();
-            int temp, temp2=0;
+            int temp, temp2 = 0, temp3 = 0;
 
             if (arrayLength == 2) {
                 System.out.println(firstValue + " " + secondValue);
             } else {
-                temp = Math.max(firstValue, secondValue) - Math.min(firstValue, secondValue);
+                if (Math.max(firstValue, secondValue) % arrayLength == 0) {
+                    temp = Math.max(firstValue, secondValue) / arrayLength;
+                    for (int i = 1; i <= arrayLength; i++) {
+                        if (i == arrayLength) System.out.println(temp * i);
+                        else System.out.print(temp * i + " ");
+                    }
+                } else {
+                    temp = Math.max(firstValue, secondValue) - Math.min(firstValue, secondValue);
+                    for (int i = 2; i < arrayLength - 2; i++) {
+                        if (temp % i == 0) {
+                            temp2 = i;
+                            temp3 = temp / i;
+                        }
+                    }
 
+                    for (int i = 0; i < arrayLength; i++) {
+                        temp = arrayLength - temp2 - i;
+                        if ((firstValue - (temp * temp3)) > 0) {
+                            temp = firstValue - (temp * temp3);
+                            break;
+                        }
+                    }
 
+                    for (int i = 0; i < arrayLength; i++) {
+                        if (i == arrayLength - 1) System.out.println((temp + (temp3 * i)));
+                        else System.out.print((temp + (temp3 * i)) + " ");
+                    }
+                }
             }
         }
     }
