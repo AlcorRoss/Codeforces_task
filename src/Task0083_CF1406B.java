e6306c6d718f668ed15167e8416105bce7317316
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-22 13:51:22 +0300

    -

--- src/Task0083_CF1406B.java
+++ src/Task0083_CF1406B.java
@@ -9,7 +9,7 @@ public class Task0083_CF1406B {
             int temp = 0;
             List<Integer> values = new ArrayList<>();
             List<Integer> resultList = new ArrayList<>();
-            boolean flag = true, flag2 = false, flag3 = false;
+            boolean flag = false, flag2 = false, flag3 = false;
 
             for (int i = 0; i < numberOfValues; i++) values.add(in.nextInt());
 
@@ -23,12 +23,17 @@ public class Task0083_CF1406B {
             for (int i = values.size() - 1; i > values.size() - 6; i--) {
                 resultList.add(values.get(i));
                 if (values.get(i) < 0) temp++;
-                if (values.get(i) == 0) flag = false;
+                if (values.get(i) == 0) flag = true;
+            }
+
+            if (flag) {
+                System.out.println(resultList.stream().reduce((acc, i) -> acc * i).get().longValue());
+                continue;
             }
 
             for (int i = 0; i < 5; i++) values.remove(values.size() - 1);
 
-            if (temp % 2 != 0 && flag) {
+            if (temp % 2 != 0) {
                 for (int value : values) {
                     if (value < 0) flag2 = true;
                     if (value > 0) flag3 = true;
@@ -45,9 +50,10 @@ public class Task0083_CF1406B {
                         break;
                     }
                 }
-
-                System.out.println(resultList.stream().reduce((acc, i) -> acc * i).get().longValue());
             }
+
+            System.out.println(resultList.stream().reduce((acc, i) -> acc * i).get().longValue());
         }
     }
 }
+
