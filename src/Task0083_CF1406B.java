6cafc00fdd4488bcb18430dad1eda1d0a4f3af60
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-22 12:17:11 +0300

    -

--- src/Task0083_CF1406B.java
+++ src/Task0083_CF1406B.java
@@ -8,7 +8,7 @@ public class Task0083_CF1406B {
             int numberOfValues = in.nextInt();
             int temp = 0;
             List<Integer> values = new ArrayList<>();
-            List<Integer> tempList = new ArrayList<>();
+            List<Integer> resultList = new ArrayList<>();
             boolean flag = false;
 
             for (int i = 0; i < numberOfValues; i++) {
@@ -24,27 +24,21 @@ public class Task0083_CF1406B {
 
             values.sort(Comparator.comparingInt(Math::abs));
             for (int i = values.size() - 1; i > values.size() - 6; i--) {
-                tempList.add(values.get(i));
+                resultList.add(values.get(i));
                 if (values.get(i) < 0) temp++;
             }
 
-            if (temp % 2 != 0 && flag) {
-                for (int i = tempList.size() - 1; i >= 0; i--) {
-                    if (values.get(i) < 0) {
-                        temp = i;
-                        break;
-                    }
-                }
+            for (int i = 0; i < 5; i++) values.remove(values.size() - 1);
 
-                for (int i = values.size() - 6; i >= 0; i--) {
-                    if (values.get(i) >= 0) {
-                        tempList.set(temp, values.get(i));
-                        break;
-                    }
+            if (temp % 2 != 0 && flag) {
+                if (resultList.get(4) < 0) {
+                    resultList.set(4, Collections.max(values));
+                } else if (resultList.get(4) > 0) {
+                    resultList.set(4, Collections.min(values));
                 }
             }
 
-            System.out.println(tempList.stream().reduce((acc, i) -> acc * i).get().longValue());
+            System.out.println(resultList.stream().reduce((acc, i) -> acc * i).get().longValue());
         }
     }
 }
