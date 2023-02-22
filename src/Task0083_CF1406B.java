ea57477836ff4397c49ec277f85edb49b9253ed1
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-22 14:56:58 +0300

    -

--- src/Task0083_CF1406B.java
+++ src/Task0083_CF1406B.java
@@ -8,12 +8,12 @@ public class Task0083_CF1406B {
 
         while (numberOfTests-- > 0) {
             int n = in.nextInt();
-            long[] a = new long[n];
+            long[] v = new long[n];
 
             for (int i = 0; i < n; i++)
-                a[i] = in.nextLong();
-            Arrays.sort(a);
-            System.out.println(Math.max(Math.max(a[0] * a[1] * a[2] * a[3] * a[n - 1], a[0] * a[1] * a[n - 1] * a[n - 2] * a[n - 3]), a[n - 1] * a[n - 2] * a[n - 3] * a[n - 4] * a[n - 5]));
+                v[i] = in.nextLong();
+            Arrays.sort(v);
+            System.out.println(Math.max(Math.max(v[0] * v[1] * v[2] * v[3] * v[n - 1], v[0] * v[1] * v[n - 1] * v[n - 2] * v[n - 3]), v[n - 1] * v[n - 2] * v[n - 3] * v[n - 4] * v[n - 5]));
         }
     }
 }
