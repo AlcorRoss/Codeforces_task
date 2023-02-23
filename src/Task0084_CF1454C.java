562d0e50b3aadfc94637c75d0c983e6f08db6afb
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-23 16:14:47 +0300

    -

---
+++ src/Task0084_CF1454C.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class Task0084_CF1454C {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numberOfTests = in.nextInt();
+
+        while (numberOfTests-- > 0) {
+            int numberOfValues = in.nextInt();
+            int[] values = new int[numberOfValues];
+            List<Integer> valuesList = new ArrayList<>();
+            Map<Integer, Integer> resultQuantity = new LinkedHashMap<>();
+
+            for (int i = 0; i < numberOfValues; i++) values[0] = in.nextInt();
+
+            valuesList.add(values[0]);
+            for (int i = 1; i < values.length; i++)
+                if (values[i] != values[i - 1]) valuesList.add(values[i]);
+
+            for (int value : valuesList) resultQuantity.merge(value, 1, Integer::sum);
+
+            int[] result = new int[resultQuantity.size()];
+            int temp = 0;
+            for (int value : resultQuantity.values()) {
+                result[temp] = value;
+                temp++;
+            }
+
+            result[0] -= 1;
+            result[result.length - 1] -= 1;
+
+            for (int i = 1; i < result.length - 1; i++) result[i] += 1;
+
+            temp = 0;
+            for (int value : result) temp = Math.min(temp, value);
+
+            System.out.println(temp);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
