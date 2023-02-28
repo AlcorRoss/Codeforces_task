fb9ca9247482857e8344cbcd955ccd0175af89b5
AlcorRoss <sokolovdenic1@mail.ru>
2023-02-28 11:55:56 +0300

    -

---
+++ src/Task0092_CF405B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Task0092_CF405B {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numberOfDom = in.nextInt();
+        String line = in.next();
+        char[] dom = line.toCharArray();
+
+    }
+}
