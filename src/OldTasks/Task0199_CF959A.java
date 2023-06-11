1a8c0e5e28b49d9134042e1db19494c2fba601d6
AlcorRoss <sokolovdenic1@mail.ru>
2023-06-12 00:44:45 +0300

    Add a solution to the task 959A

---
+++ src/OldTasks/Task0199_CF959A.java
@@ -0,0 +1,11 @@
+package OldTasks;
+
+import java.util.Scanner;
+
+public class Task0199_CF959A {
+    public static void main(String[] args) {
+        var in = new Scanner(System.in);
+        if(in.nextInt()%2==0) System.out.println("Mahmoud");
+        else System.out.println("Ehab");
+    }
+}
